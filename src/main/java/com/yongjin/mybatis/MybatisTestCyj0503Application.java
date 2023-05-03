fa4f63227c0219b6fb3b541a0c4a6ea7d91b3dd9
7CYJ <jojo3528@gmail.net>
2023-05-03 17:37:43 +0900

    springboot test

---
+++ src/main/java/com/yongjin/mybatis/MybatisTestCyj0503Application.java
@@ -0,0 +1,13 @@
+package com.yongjin.mybatis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MybatisTestCyj0503Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MybatisTestCyj0503Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/yongjin/mybatis/MybatisTestCyj0503ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yongjin.mybatis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MybatisTestCyj0503ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
